9e7c63e675253c43e079cc15276a59d642cad19f
joacojachow <joacojachow@hotmail.com>
2025-11-07 12:05:10 -0300

    footer improved

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -2,10 +2,10 @@ import React, { memo } from 'react';
 
 const Footer = memo(() => {
   return (
-    <footer className="fixed inset-x-0 bottom-0 w-full py-1 bg-white border-gray-200 rounded shadow-md">
-      <p className='flex items-center gap-1 mx-auto font-medium text-gray-700 max-w-7xl'>
+    <footer className="fixed inset-x-0 bottom-0 w-full py-1 bg-white dark:bg-dark-secondary border-gray-200 dark:border-gray-600 rounded shadow-md transition-colors duration-300">
+      <p className='flex items-center gap-1 mx-auto font-medium text-gray-700 dark:text-dark-text max-w-7xl'>
         <span>© Made by</span>
-        <a target={'_blank'} rel="noreferrer" href="https://www.linkedin.com/in/joaquin-jachow/">Joaquin Jachow</a>
+        <a target={'_blank'} rel="noreferrer" href="https://www.linkedin.com/in/joaquin-jachow/" className="hover:text-blue-600 dark:hover:text-blue-400 transition-colors duration-300">Joaquin Jachow</a>
       </p>
     </footer>
   )
