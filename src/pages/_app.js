da526934f89959a87dbbcee66464579244f400da
joacojachow <joacojachow@hotmail.com>
2025-12-25 16:51:12 -0300

    all finish

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,16 +1,20 @@
 import '@/styles/globals.css'
 import Head from 'next/head'
 import { Poppins } from 'next/font/google'
+import { ThemeProvider } from '@/contexts/ThemeContext'
+
 const poppins = Poppins({ subsets: ['latin'], weight: ['400', '500', '600', '700', '800'] })
 
 export default function App({ Component, pageProps }) {
   return (
-    <main className={`${poppins.className}`}>
-      <Head>
-        <title>Weather App</title>
-        <link rel='icon' href='/favicon.png' />
-      </Head>
-      <Component {...pageProps} />
-    </main>
+    <ThemeProvider>
+      <main className={`${poppins.className}`}>
+        <Head>
+          <title>Weather App</title>
+          <link rel='icon' href='/favicon.png' />
+        </Head>
+        <Component {...pageProps} />
+      </main>
+    </ThemeProvider>
   )
 }
